fc38cc160b76e77f33aa7b443cb1223e3f444900
Piyush Sinha <Piyush.Sinha2@ibm.com>
2026-07-19 11:46:30 +0530

    complete module 3 collections

---
+++ lessons/03_collections/exercises.py
@@ -0,0 +1,19 @@
+### Lists
+
+numbers = [1, 2, 3]
+
+
+### Tuples
+point = (10, 20)
+
+
+### Sets
+languages = {"Python", "Java"}
+
+
+
+### Dictionaries
+person = {
+    "name": "Piyush",
+    "age": 30,
+}
